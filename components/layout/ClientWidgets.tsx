215079c122e97052c9ffbe761c86700fee682eb1
elevateforhumanity <elevateforhumanity@gmail.com>
2026-01-26 07:57:24 +0000

    fix: disable AvatarChatBar voiceover loop
    Remove all avatar/voiceover components from ClientWidgets.
    
    Co-authored-by: Ona <no-reply@ona.com>

--- components/layout/ClientWidgets.tsx
+++ components/layout/ClientWidgets.tsx
@@ -3,22 +3,7 @@
 // Client-only widgets that don't block page rendering
 // These load after the page is visible
 
-import dynamic from 'next/dynamic';
-
-// Lazy load non-critical client components
-const AvatarChatBar = dynamic(() => import('@/components/AvatarChatBar'), {
-  ssr: false,
-});
-
-const LiveChatWidget = dynamic(() => import('@/components/LiveChatWidget'), {
-  ssr: false,
-});
-
+// All avatar/voiceover components disabled
 export default function ClientWidgets() {
-  return (
-    <>
-      <AvatarChatBar />
-      {/* <LiveChatWidget /> */}
-    </>
-  );
+  return null;
 }
