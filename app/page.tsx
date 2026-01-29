dbba80607a648bb16445215cce498b5a3fa00ef2
elevateforhumanity <elevateforhumanity@gmail.com>
2026-01-29 05:40:38 +0000

    Revert homepage video back to original
    Co-authored-by: Ona <no-reply@ona.com>

--- app/page.tsx
+++ app/page.tsx
@@ -124,7 +124,7 @@ export default function HomePage() {
       </section>
 
       {/* AVATAR */}
-      <PageAvatar videoSrc="/videos/elevate-overview-with-narration.mp4" title="Learn About Elevate" />
+      <PageAvatar videoSrc="/videos/avatars/home-welcome.mp4" title="Welcome to Elevate" />
 
       {/* ABOUT with play button overlay */}
       <section className="py-16 lg:py-24 bg-slate-50">
